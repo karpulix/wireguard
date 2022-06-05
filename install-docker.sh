ea47595d1e86ce9e9518fbd4250347456e34d83f
Alexander Karpov <karpulix@ya.ru>
2022-06-05 14:26:49 +0400

    install-docker.sh was added

---
+++ install-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Set up the repository
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker’s official GPG key:
+sudo mkdir -p /etc/apt/keyrings
+
+# Use the following command to set up the repository:
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
